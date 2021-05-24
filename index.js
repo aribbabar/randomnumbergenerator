85d57ccf0d7a56829624d5333131790219363843
Arib Babar <aribbabar100@gmail.com>
2021-05-24 01:20:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let lower;
+let upper;
+
+const generate = document.getElementById("generate");
+const clear = document.getElementById("clear");
+const result = document.getElementById("result");
+
+const invalid = document.getElementById("invalid");
+
+if(generate != null) {
+    generate.addEventListener("click", generateRandomNumber)
+}
+
+if(clear != null) {
+    clear.addEventListener("click", clearValues);
+}
+
+function generateRandomNumber() {
+    lower = parseInt(document.getElementById("lower-bound").value);
+    upper = parseInt(document.getElementById("upper-bound").value);
+
+    if(upper < lower) {
+        invalid.innerHTML = "Upper bound can not be lower than lower bound!";
+        invalid.style.display = "block"
+
+        return;
+    }
+
+    result.value = Math.floor(Math.random() * (upper - lower + 1)) + lower;
+}
+
+function clearValues() {
+    document.getElementById("lower-bound").value = "";
+    document.getElementById("upper-bound").value = "";
+    document.getElementById("result").value = "";
+
+    invalid.style.display = "none"
+}
